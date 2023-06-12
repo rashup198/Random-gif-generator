a5045386183b85d6b5bdb06e44def3dffb1b7a58
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-12 20:10:45 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Tag from './components/Tag';
 
 export default function App() {
   return(
-    <div className="w-full h-screen flex flex-col background ">
+    <div className="w-full max-h-screen flex flex-col background ">
       <h1 className="bg-white rounded-lg  w-11/12 text-center ml-[50px] mt-5 items-center text-lg font-bold ">RANDOM GIFS</h1>
-      <div className="Flex flex-col items-center justify-center gap-y-10 mt-[30px]">
+      <div className="Flex flex-col items-center justify-center gap-y-10 mt-[30px] background">
         <Random/>
+        <Tag></Tag>
         
       </div>
     </div>
