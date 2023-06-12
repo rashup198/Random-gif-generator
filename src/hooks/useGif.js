e522ad03925ad350b9445c1510ea3bfad2cedb05
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-12 20:40:42 +0530

    updated useGif.js

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,5 +1,38 @@
-const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const useGif = (tag) => {};
 
-export default useGif;
+const apikey='ePyrN1QX8SD8bXPGKQt0jw9OeYy3bkT5';
+
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${apikey}`;
+
+
+const useGif = (tag) => {
+    const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState('false');
+
+
+
+    
+  
+    async function fetchData(tag){
+      setLoading(true);
+      
+      const output = await axios.get(tag ? `${url}&tag=${tag}` : (url));
+      console.log(output);
+      const imageSource= output.data.data.images.downsized_large.url;
+      console.log(imageSource);
+      setGif(imageSource);
+      setLoading(false);
+    }
+    useEffect(()=>{
+      fetchData("car");
+    },[] );
+
+    return{gif, loading, fetchData};
+  
+}
+
+export default useGif
