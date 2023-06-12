6402da6eb4ca7d258bcfc8a4162cad4cf653246e
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-12 19:41:17 +0530

    updated spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div>
+        <div class="custom-loader"></div>
+
+        
+      
+    </div>
+  )
+}
+
+export default Spinner
+
+    
